d57469209bbbd1b72a30fa8697f70b585e309340
Galliss13 <matheusgalindo1313@gmail.com>
2022-11-28 13:48:39 -0300

    feat: getCartProducts

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   postProductIntoCart,
   deleteProductInCart,
+  getCartProducts,
 } from "../controllers/client.controller";
 import { authTokenValidation } from "../middlewares/authValidation.middleware.js";
 
 const router = Router();
 
+router.get("/cart", authTokenValidation, getCartProducts)
 router.post("/cart", authTokenValidation, postProductIntoCart);
 router.delete("/cart:productId",authTokenValidation, deleteProductInCart);
 
